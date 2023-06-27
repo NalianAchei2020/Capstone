60c3514a8b3f28675100af12fb61a93cd0af551b
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-06-27 18:36:48 +0100

    add linters

--- file.js
+++ file.js
@@ -1,83 +1,83 @@
+/* eslint-disable no-plusplus */
 const Menu = document.getElementById('menu');
 const closeMenu = document.getElementById('close');
 const nav = document.querySelector('.nav-items');
-Menu.addEventListener('click', ()=>{
-    nav.style.visibility = 'visible';
-    closeMenu.style.display = 'block';
-    Menu.style.display = 'none';
-})
-closeMenu.addEventListener('click', ()=>{
-    nav.style.visibility = 'hidden';
-    closeMenu.style.display = 'none';
-    Menu.style.display = 'block';
-})
-
+Menu.addEventListener('click', () => {
+  nav.style.visibility = 'visible';
+  closeMenu.style.display = 'block';
+  Menu.style.display = 'none';
+});
+closeMenu.addEventListener('click', () => {
+  nav.style.visibility = 'hidden';
+  closeMenu.style.display = 'none';
+  Menu.style.display = 'block';
+});
 
 const data = [
-    {
-        name:"Milton Njonya",
-        profession: "CEO and Founder of  WEBDEV MENTORS",
-        Image: "Images/speaker01.png",
-        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad"
-    },
-    {
-        name:"Micheal Jean",
-        profession: "Senoir software Engineer at Microsoft",
-        Image: "Images/speaker02.png",
-        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad"
-    },
-    {
-        name:"Mousa YAYA",
-        profession: "Senoir Developer at Microsoft",
-        Image: "Images/speaker03.png",
-        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad"
-    },
-    {
-        name:"Mousa YAYA",
-        profession: "Professor of Information Technology at Harvard University",
-        Image: "Images/speaker04.png",
-        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad"
-    },
-    {
-        name:"Mousa YAYA",
-        profession: "CEO and Founder of  WEBDEV MENTORS",
-        Image: "Images/speaker05.png",
-        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad"
-    },
-    {
-        name:"Mousa YAYA",
-        profession: "CEO and Founder of  WEBDEV MENTORS",
-        Image: "Images/speaker06.png",
-        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad"
-    }
-]
+  {
+    name: 'Milton Njonya',
+    profession: 'CEO and Founder of  WEBDEV MENTORS',
+    Image: 'Images/speaker01.png',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad',
+  },
+  {
+    name: 'Micheal Jean',
+    profession: 'Senoir software Engineer at Microsoft',
+    Image: 'Images/speaker02.png',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad',
+  },
+  {
+    name: 'Mousa YAYA',
+    profession: 'Senoir Developer at Microsoft',
+    Image: 'Images/speaker03.png',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad',
+  },
+  {
+    name: 'Mousa YAYA',
+    profession: 'Professor of Information Technology at Harvard University',
+    Image: 'Images/speaker04.png',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad',
+  },
+  {
+    name: 'Mousa YAYA',
+    profession: 'CEO and Founder of  WEBDEV MENTORS',
+    Image: 'Images/speaker05.png',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad',
+  },
+  {
+    name: 'Mousa YAYA',
+    profession: 'CEO and Founder of  WEBDEV MENTORS',
+    Image: 'Images/speaker06.png',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad',
+  },
+];
 
 const btnMore = document.getElementById('btn-more');
 const btnHide = document.getElementById('btn-hide');
-document.querySelector('.speaker').innerHTML = data.map((item, index)=>{
-    let className = '';
-   if(index !== 0 && index !== 1){
-     className = 'hide-card';
-   }
+document.querySelector('.speaker').innerHTML = data.map((item, index) => {
+  let className = '';
+  if (index !== 0 && index !== 1) {
+    className = 'hide-card';
+  }
 
-   btnMore.addEventListener('click', ()=>{
-   const hideCard = document.querySelectorAll('.hide-card');
-   for(let i = 0; i< hideCard.length; i++){
-    hideCard[i].style.display = 'flex';
-   }
-   btnHide.style.display = 'block';
-   btnMore.style.display = 'none'
-   })
-
-   btnHide.addEventListener('click', ()=>{
+  btnMore.addEventListener('click', () => {
     const hideCard = document.querySelectorAll('.hide-card');
-    for(let i = 0; i< hideCard.length; i++){
-     hideCard[i].style.display = 'none';
+    for (let i = 0; i < hideCard.length; i++) {
+      hideCard[i].style.display = 'flex';
+    }
+    btnHide.style.display = 'block';
+    btnMore.style.display = 'none';
+  });
+
+  btnHide.addEventListener('click', () => {
+    const hideCard = document.querySelectorAll('.hide-card');
+    for (let i = 0; i < hideCard.length; i++) {
+      hideCard[i].style.display = 'none';
     }
     btnHide.style.display = 'none';
-    btnMore.style.display = 'block'
-    })
-    return `
+    btnMore.style.display = 'block';
+  });
+  return `
     <div class = "container">
        <div class = "speaker-card ${className}">
          <div class="image-container">
@@ -90,5 +90,5 @@ document.querySelector('.speaker').innerHTML = data.map((item, index)=>{
             </div>
             </div>
             </div>
-    `
-})
+    `;
+});
